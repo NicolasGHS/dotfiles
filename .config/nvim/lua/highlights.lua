67859733d09f965cc78052a138e13d7fec35fcc8
NicolasGHS <nicoghyselincks@outlook.be>
2024-10-01 22:13:18 +0200

    add nvim

---
+++ .config/nvim/lua/highlights.lua
@@ -0,0 +1,8 @@
+---------------------------
+-- COLORSCHEMES
+---------------------------
+
+-- Uncomment just ONE of the following colorschemes!
+-- local ok, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
+local ok, _ = pcall(vim.cmd, "colorscheme horizon")
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,56 @@
+local status, packer = pcall(require, "packer")
+if not status then
+	print("Packer is not installed")
+	return
+end
+
+-- Reloads Neovim after whenever you save plugins.lua
+vim.cmd([[ augroup packer_user_config
+      autocmd!
+     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup END
+]])
+
+packer.startup(function(use)
+    use("wbthomason/packer.nvim")					-- Packer
+    use {
+    	'vimwiki/vimwiki', 						-- Vimwiki
+    	config = function()
+        	vim.g.vimwiki_list = {
+            		{
+                	path = '/home/xx/Documents/singularityOffice/wiki',
+                	syntax = 'markdown',
+                	ext = '.md',
+            		}
+        	}
+    	end
+    }						
+    use("scrooloose/nerdtree")						-- NerdTree
+    use {
+  	'nvim-lualine/lualine.nvim',					-- LuaLine
+  	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
+  	'nvim-telescope/telescope.nvim', tag = '0.1.8',                 -- Telescope
+	-- or                            , branch = '0.1.x',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+  	'nvimdev/dashboard-nvim',					-- Dashboard
+  	event = 'VimEnter',
+  	config = function()
+    		require('dashboard').setup {
+      		-- config
+    		}
+  	end,
+  	requires = {'nvim-tree/nvim-web-devicons'}
+    }
+    use { "catppuccin/nvim", as = "catppuccin" }			-- Catppuccin
+    use {"akinsho/horizon.nvim", tag = "*"}				-- Horizon
+
+
+end)
+
+require("lualine").setup()
+
+
