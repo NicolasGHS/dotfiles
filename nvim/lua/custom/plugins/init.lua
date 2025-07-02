d6de5ddcc4914b7e9096195b6881e0652c4d5c3d
NicolasGHS <nicoghyselincks@outlook.be>
2025-07-02 03:52:31 +0200

    chore: tmux not hidden

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -30,23 +30,6 @@ return {
     --   enable_close_on_slash = false, -- Auto close on trailing </
     -- },
   },
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*', -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = 'markdown',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    opts = {
-      workspaces = {
-        {
-          name = 'notes',
-          path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes/',
-        },
-      },
-    },
-  },
 
   -- Nordic theme
   {
